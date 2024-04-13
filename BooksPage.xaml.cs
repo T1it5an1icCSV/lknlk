3655fb1a5a7a33ab358509a29467f0c7eca15091
T1it5an1icCSV <145016510+T1it5an1icCSV@users.noreply.github.com>
2024-04-13 15:06:14 +0300

    Add files via upload

---
+++ BooksPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Practica1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Practica1.Practica1DataSetTableAdapters;
+
+namespace Practica1
+{
+    /// <summary>
+    /// Логика взаимодействия для BooksPage.xaml
+    /// </summary>
+    public partial class BooksPage : Page
+    {
+        BooksTableAdapter practica = new BooksTableAdapter();
+        public BooksPage()
+        {
+            InitializeComponent();
+            BooksDataGrid.ItemsSource = practica.GetData();
+        }
+        private void Exit_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Window.GetWindow(this).Close();
+        }
+    }
+}
+
+  
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Practica1.Practica1DataSetTableAdapters;
+
+namespace Practica1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Authors_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Authors AuthorsPage = new Authors();
+            this.Content = AuthorsPage;
+        }
+
+        private void Books_Button_Click(object sender, RoutedEventArgs e)
+        {
+            BooksPage BooksPage1 = new BooksPage();
+            this.Content = BooksPage1;
+        }
+    }
+}
